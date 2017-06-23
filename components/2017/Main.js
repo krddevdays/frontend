59d319b4f6b7467d60815cb309c79434eac7e7ff
aivanov <aivanov@oneincsystems.com>
2017-06-23 12:04:28 +0300

    Copy 2016 template
    Update dates and addresses

---
+++ components/2017/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Information from './Information'
+import Location from './Location'
+
+export default () => (
+  <main role='main'>
+    <Information />
+    <Location />
+  </main>
+)
