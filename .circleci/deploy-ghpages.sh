db0e9b3329b5b09c3238fa2c3ab7d3cafa81e47b
Mark Langovoi <marklangovoi@gmail.com>
2017-07-29 23:19:58 +0300

    Fix deploy to GitHub Pages

--- .circleci/deploy-ghpages.sh
+++ .circleci/deploy-ghpages.sh
@@ -45,7 +45,8 @@ git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages > /dev/null 2>&1
+
+git push --force --quiet "$GH_AUTH_TOKEN@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git" gh-pages > /dev/null 2>&1
 
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
