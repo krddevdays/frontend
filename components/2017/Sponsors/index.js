720f752fa34d10b13e49540ef89461181b34b946
Mark Langovoi <marklangovoi@gmail.com>
2017-08-12 12:37:20 +0300

    Add Sponsor

--- components/2017/Sponsors/index.js
+++ components/2017/Sponsors/index.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 
 import Section from '../Section'
 import Text from '../Text'
 import Button from '../Button'
+import grayscale from '../grayscale'
 
 const Heading = require('../Heading').default.withComponent('h3')
 
+const Sponsor = styled(({className, href, image, name}) => (
+  <a className={className} href={href} target='_blank'>
+    <img src={image} alt={name} title={name} height='40px' />
+  </a>
+))`
+  ${grayscale}
+  
+  display: inline-block;
+  margin: 1rem 0.5rem;
+  padding: 0;
+  text-decoration: none;
+  
+  > img {
+    vertical-align: top;
+  }
+`
+
 export default () => (
-  <ThemeProvider theme={{color: 'dark'}}>
+  <ThemeProvider theme={{color: 'light'}}>
     <Section>
       <Heading>Спонсоры</Heading>
-      <Text align='center' />
+      <Text align='center'>
+        <Sponsor
+          href='https://www.billing.ru'
+          image={require('./peter-service.svg')}
+          name='PETER-SERVICE'
+        />
+      </Text>
       <Text align='center'>
         <Button href={require('./information.pdf')}>Стать спонсором</Button>
       </Text>
