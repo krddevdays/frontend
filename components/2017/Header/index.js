dbeb524ce66d7e3ce05595dbbc02801e7daeb530
neonick <n30n1ck@gmail.com>
2017-08-19 23:22:13 +0300

    Add to calendar buttons

--- components/2017/Header/index.js
+++ components/2017/Header/index.js
@@ -101,6 +101,9 @@ const Header = () => (
         Приобрести билет
       </Button>
       <Button href='http://eepurl.com/cPFwj9' target='_blank'>Подписаться на рассылку</Button>
+      <Button href='http://addtocalendar.com/atc/ical?f=m&e[0][date_start]=2017-09-17%2010%3A00%3A00&e[0][date_end]=2017-09-17%2019%3A00%3A00&e[0][timezone]=Europe%2FMoscow&e[0][title]=Krasnodar%20Dev%20Days&e[0][description]=%D0%9A%D0%BE%D0%BD%D1%84%D0%B5%D1%80%D0%B5%D0%BD%D1%86%D0%B8%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D0%BE%D0%B2&e[0][location]=%20%D0%B3.%20%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80%2C%20%D1%83%D0%BB.%20%D0%93%D0%B8%D0%BC%D0%BD%D0%B0%D0%B7%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F%2065&e[0][organizer]=Krasnodar%20Dev%20Days&e[0][organizer_email]=mark%40krddevdays.ru&e[0][privacy]=public' target='_blank' color='gradient'>iCalendar</Button>
+      <Button href='http://addtocalendar.com/atc/google?f=m&e[0][date_start]=2017-09-17%2010%3A00%3A00&e[0][date_end]=2017-09-17%2019%3A00%3A00&e[0][timezone]=Europe%2FMoscow&e[0][title]=Krasnodar%20Dev%20Days&e[0][description]=%D0%9A%D0%BE%D0%BD%D1%84%D0%B5%D1%80%D0%B5%D0%BD%D1%86%D0%B8%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D0%BE%D0%B2&e[0][location]=%20%D0%B3.%20%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80%2C%20%D1%83%D0%BB.%20%D0%93%D0%B8%D0%BC%D0%BD%D0%B0%D0%B7%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F%2065&e[0][organizer]=Krasnodar%20Dev%20Days&e[0][organizer_email]=mark%40krddevdays.ru&e[0][privacy]=public' target='_blank' color='gradient'>Google Calendar</Button>
+
     </Buttons>
   </Container>
 )
