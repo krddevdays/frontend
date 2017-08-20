2881edf7526c641c5a3f699d4aec78ef10f03eac
Mark Langovoi <marklangovoi@gmail.com>
2017-08-20 15:05:34 +0300

    Update addtocalendar's buttons

--- components/2017/Header/index.js
+++ components/2017/Header/index.js
@@ -6,8 +6,22 @@ import { hybrid } from 'react-syntax-highlighter/dist/styles'
 
 import Image from '../../Image'
 import Button from '../Button'
+import Text from '../Text'
 
 registerLanguage('javascript', js)
+
+const calendarQueryString = require('querystring').stringify({
+  f: 'm',
+  'e[0][date_start]': '2017-09-17 10:00:00',
+  'e[0][date_end]': '2017-09-17 20:00:00',
+  'e[0][timezone]': 'Europe/Moscow',
+  'e[0][title]': 'Krasnodar Dev Days #2',
+  'e[0][description]': 'Конференция для разработчиков',
+  'e[0][location]': 'г. Краснодар, ул. Гимназическая 65',
+  'e[0][organizer]': 'Krasnodar Dev Days',
+  'e[0][organizer_email]': 'mark@krddevdays.ru',
+  'e[0][privacy]': 'public'
+})
 
 const Container = styled.main`
   flex-shrink: 0;
@@ -36,13 +50,17 @@ const Information = styled.div`
 `
 
 const Buttons = styled.div`
-  margin-top: 2rem;
+  margin: 1rem 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
 
   > ${Button} {
     margin-top: 1rem;
+  }
+
+  > ${Button}:first-child {
+    margin-top: 0;
   }
 
   @media (min-width: 768px) {
@@ -97,13 +115,27 @@ const Header = () => (
       </SyntaxHighlighter>
     </Information>
     <Buttons>
-      <Button href='https://krddevdays.timepad.ru/event/512166/?utm_refcode=4c6b52e5f714cf8bad3b8033eb3344a85090149b' color='light'>
+      <Button
+        href='https://krddevdays.timepad.ru/event/512166/?utm_refcode=4c6b52e5f714cf8bad3b8033eb3344a85090149b'
+        color='light'>
         Приобрести билет
       </Button>
       <Button href='http://eepurl.com/cPFwj9' target='_blank'>Подписаться на рассылку</Button>
-      <Button href='http://addtocalendar.com/atc/ical?f=m&e[0][date_start]=2017-09-17%2010%3A00%3A00&e[0][date_end]=2017-09-17%2019%3A00%3A00&e[0][timezone]=Europe%2FMoscow&e[0][title]=Krasnodar%20Dev%20Days&e[0][description]=%D0%9A%D0%BE%D0%BD%D1%84%D0%B5%D1%80%D0%B5%D0%BD%D1%86%D0%B8%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D0%BE%D0%B2&e[0][location]=%20%D0%B3.%20%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80%2C%20%D1%83%D0%BB.%20%D0%93%D0%B8%D0%BC%D0%BD%D0%B0%D0%B7%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F%2065&e[0][organizer]=Krasnodar%20Dev%20Days&e[0][organizer_email]=mark%40krddevdays.ru&e[0][privacy]=public' target='_blank' color='gradient'>iCalendar</Button>
-      <Button href='http://addtocalendar.com/atc/google?f=m&e[0][date_start]=2017-09-17%2010%3A00%3A00&e[0][date_end]=2017-09-17%2019%3A00%3A00&e[0][timezone]=Europe%2FMoscow&e[0][title]=Krasnodar%20Dev%20Days&e[0][description]=%D0%9A%D0%BE%D0%BD%D1%84%D0%B5%D1%80%D0%B5%D0%BD%D1%86%D0%B8%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D0%BE%D0%B2&e[0][location]=%20%D0%B3.%20%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80%2C%20%D1%83%D0%BB.%20%D0%93%D0%B8%D0%BC%D0%BD%D0%B0%D0%B7%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F%2065&e[0][organizer]=Krasnodar%20Dev%20Days&e[0][organizer_email]=mark%40krddevdays.ru&e[0][privacy]=public' target='_blank' color='gradient'>Google Calendar</Button>
-
+    </Buttons>
+    <Text align='center'>Добавить в календарь:</Text>
+    <Buttons>
+      <Button
+        href={`http://addtocalendar.com/atc/ical?${calendarQueryString}`}
+        target='_blank'
+        textTransform='inherit'>
+        iCalendar
+      </Button>
+      <Button
+        href={`http://addtocalendar.com/atc/google?${calendarQueryString}`}
+        target='_blank'
+        textTransform='inherit'>
+        Google Calendar
+      </Button>
     </Buttons>
   </Container>
 )
