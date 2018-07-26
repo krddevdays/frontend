91cd7465a72bc0616e1401b70b24aefb1c7ba3aa
Mark Langovoi <marklangovoi@gmail.com>
2018-07-26 23:20:39 +0300

    Add firebase and react-firestore for load topics of round table

--- src/components/BorderedBox/index.js
+++ src/components/BorderedBox/index.js
@@ -1,16 +1,9 @@
 import styled from 'styled-components'
 import Box from '../Box'
-import {borders, borderColor} from 'styled-system'
 
-const BorderedBox = styled(Box)([],
-  borders,
-  borderColor,
-)
-
-BorderedBox.defaultProps = {
-  bg: 'white',
-  border: '6px solid',
-  borderColor: '#252525',
-}
+const BorderedBox = styled(Box)`
+  border: 6px solid #252525;
+  background: white;
+`
 
 export default BorderedBox
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,16 +1,22 @@
 import styled, {css} from 'styled-components'
 import tag from 'clean-tag'
-import {flex, space, display} from 'styled-system'
+import {flex, space, display, width} from 'styled-system'
 
 const active = css`
   background: #252525;
   color: white;
 `
 
-const Button = styled(tag)`
+const Button = styled(tag).attrs({
+  blacklist: [
+    ...tag.defaultProps.blacklist,
+    'active',
+  ],
+})`
   ${display}
   ${space}
   ${flex}
+  ${width}
   
   font-size: 18px;
   line-height: 22px;
@@ -24,19 +30,26 @@ const Button = styled(tag)`
   color: #252525;
   padding: 13px 27px;
   
+  ${props => props.active && active};
+  
   &:hover {
     cursor: pointer;
     box-shadow: -10px 10px 0 #B07EC5;
   }
   
+  &:focus {
+    outline: none;
+    box-shadow: -10px 10px 0 #B07EC5;
+  }
+  
   &:active {
     cursor: pointer;
-    ${active}
+    ${active};
   }
 `
 
 Button.defaultProps = {
-  is: 'button'
+  is: 'button',
 }
 
 export default Button
