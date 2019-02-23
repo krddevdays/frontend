60583a137670f4c104460754e4718a6559a91d9b
Mark Langovoi <marklangovoi@gmail.com>
2019-02-23 22:27:19 +0300

    Add filter by events types (resolve #56)

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,14 +1,47 @@
 import * as React from 'react';
-import { NextFunctionComponent } from 'next';
+import { NextContext, NextFunctionComponent } from 'next';
 import EventCard, { Event } from '../../components/EventCard/EventCard';
 import * as api from '../../api';
 import Head from 'next/head';
+import Link from 'next/link';
+import classNames from 'classnames';
+import * as url from 'url';
+
+const types = [
+    {
+        id: 'conf' as 'conf',
+        title: 'Conf'
+    },
+    {
+        id: 'frontend' as 'frontend',
+        title: 'Frontend'
+    },
+    {
+        id: 'backend' as 'backend',
+        title: 'Backend'
+    },
+    {
+        id: 'python' as 'python',
+        title: 'Python'
+    }
+];
+
+type EventType = typeof types extends Array<{ id: infer U }> ? U : never;
 
 type EventsPageProps = {
     events: Event[];
+    types?: EventType[];
 };
 
-const EventsPage: NextFunctionComponent<EventsPageProps> = props => {
+const EventsPage: NextFunctionComponent<
+    EventsPageProps,
+    EventsPageProps,
+    NextContext & {
+        query: {
+            types?: EventType[] | EventType;
+        };
+    }
+> = props => {
     return (
         <div className="pt-3">
             <Head>
@@ -18,9 +51,57 @@ const EventsPage: NextFunctionComponent<EventsPageProps> = props => {
                 <h1 className="h2">События</h1>
             </div>
             <div className="row my-3">
-                {props.events.map((event, i) => {
+                <div className="col col-12">
+                    <div className="btn-group" role="group">
+                        {typeof props.types === 'undefined' ? (
+                            <span className="btn btn-outline-secondary active">Все</span>
+                        ) : (
+                            <Link replace={true} href="/events">
+                                <a className="btn btn-outline-secondary">Все</a>
+                            </Link>
+                        )}
+                        {types.map((type, i) => {
+                            const nextTypes = new Set<EventType>();
+
+                            if (typeof props.types !== 'undefined') {
+                                props.types.forEach(type => nextTypes.add(type));
+                            }
+
+                            const isActive = nextTypes.has(type.id);
+
+                            nextTypes[isActive ? 'delete' : 'add'](type.id);
+
+                            return (
+                                <Link
+                                    replace={true}
+                                    href={url.format({
+                                        pathname: '/events',
+                                        query:
+                                            nextTypes.size === types.length
+                                                ? undefined
+                                                : {
+                                                      types: Array.from(nextTypes)
+                                                  }
+                                    })}
+                                    key={i}
+                                >
+                                    <a
+                                        className={classNames('btn', 'btn-outline-secondary', {
+                                            active: isActive
+                                        })}
+                                    >
+                                        {type.title}
+                                    </a>
+                                </Link>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+            <div className="row my-3">
+                {props.events.map(event => {
                     return (
-                        <div className="col col-12 col-lg-4 my-2" key={i}>
+                        <div className="col col-12 col-lg-4 my-2" key={event.id}>
                             <EventCard {...event} className="h-100" />
                         </div>
                     );
@@ -31,8 +112,31 @@ const EventsPage: NextFunctionComponent<EventsPageProps> = props => {
 };
 
 EventsPage.getInitialProps = async ctx => {
+    const types = typeof ctx.query.types === 'string' ? [ctx.query.types] : ctx.query.types;
+    const events = await api.events(ctx.req);
+
     return {
-        events: await api.events(ctx.req)
+        types,
+        events:
+            typeof types === 'undefined'
+                ? events
+                : events.filter(event =>
+                      types.some(type => {
+                          switch (type) {
+                              case 'frontend':
+                                  return event.name.startsWith('Krasnodar Frontend');
+                              case 'backend':
+                                  return event.name.startsWith('Krasnodar Backend');
+                              case 'python':
+                                  return event.name.startsWith('Krasnodar Python');
+                              case 'conf':
+                                  return event.name.startsWith('Krasnodar Dev Days');
+                              default:
+                                  ((value: never) => value)(type);
+                                  return false;
+                          }
+                      })
+                  )
     };
 };
 
