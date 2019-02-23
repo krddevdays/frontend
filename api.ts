20d7bce6011e8b61ccdb06ccf43d88d82deca542
Mark Langovoi <marklangovoi@gmail.com>
2019-02-23 13:58:27 +0300

    Fix `query` formatting in API

--- api.ts
+++ api.ts
@@ -1,17 +1,34 @@
 import fetch from 'cross-fetch';
-import * as url from 'url';
-import { EventRequest, EventResponse, EventsRequest, EventsResponse } from './typings/timepad';
+import { format as urlFormat } from 'url';
+import { EventResponse, EventsRequest, EventsResponse } from './typings/timepad';
 import * as express from 'express';
 import * as http from 'http';
-import { UrlObject } from 'url';
+import * as queryString from 'query-string'
 
-function createUrl({ pathname, query, req }: Pick<UrlObject, 'pathname' | 'query'> & { req?: http.IncomingMessage }) {
-    return url.format({
-        protocol: req ? (req as express.Request).protocol : window.location.protocol,
-        host: req ? req.headers.host : window.location.host,
-        pathname,
-        query
+function createUrl(context: {
+    pathname: string;
+    query?: {
+        [key: string]: any
+    },
+    req?: http.IncomingMessage
+}) {
+    const protocol = context.req ? (context.req as express.Request).protocol : window.location.protocol;
+    const host = context.req ? context.req.headers.host : window.location.host;
+    const url = urlFormat({
+        protocol,
+        host,
+        pathname: context.pathname,
     });
+
+    if(typeof context.query === 'undefined') {
+        return url;
+    }
+
+    const query = queryString.stringify(context.query, {
+        arrayFormat: 'bracket'
+    });
+
+    return `${url}?${query}`
 }
 
 export const events = async (req?: http.IncomingMessage) => {
@@ -40,11 +57,11 @@ export const events = async (req?: http.IncomingMessage) => {
         name: event.name,
         startsAt: event.starts_at.replace(/(\d{2})(\d{2})$/, '$1:$2'),
         descriptionShort: event.description_short!,
-        ticketTypes: event.ticket_types!.map(ticketType => ({
+        ticketTypes: event.ticket_types ? event.ticket_types.map(ticketType => ({
             price: ticketType.price,
             isActive: ticketType.is_active,
             requirePromocode: ticketType.is_promocode_locked
-        }))
+        })) : []
     }));
 };
 
@@ -52,10 +69,7 @@ export const event = async (id: number, req?: http.IncomingMessage) => {
     const response = await fetch(
         createUrl({
             req,
-            pathname: `/api/timepad/v1/events/${id}`,
-            query: {
-                fields: ['description_html', 'ticket_types', 'organization']
-            } as EventRequest
+            pathname: `/api/timepad/v1/events/${id}`
         })
     );
 
