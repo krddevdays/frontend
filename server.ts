f9cb9fe812089240736034ef160d9cf57f069793
Ivakhnenko Dmitry <jeetiss@yandex.ru>
2019-02-26 18:32:40 +0300

    Encapsulate TimePad API on the client (closes #53)

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as next from 'next';
-import * as proxy from 'http-proxy-middleware';
+import events from './api/events';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
@@ -8,18 +8,14 @@ const app = next({ dev });
 
 const handle = app.getRequestHandler();
 
+function errorHandler(error: Error, _: express.Request, res: express.Response, __: express.NextFunction) {
+    res.status(500).send(error.message);
+}
+
 app.prepare().then(() => {
     const server = express();
 
-    server.use(
-        proxy('/api/timepad/', {
-            changeOrigin: true,
-            target: 'https://api.timepad.ru/',
-            pathRewrite: {
-                '^/api/timepad/': '/'
-            }
-        })
-    );
+    server.use('/api/events', events).use(errorHandler);
 
     server.get('/events/:id', (req, res) => {
         return app.render(req, res, '/events/event', { id: req.params.id });
