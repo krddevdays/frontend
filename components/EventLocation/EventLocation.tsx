52f0b57599a68d77d4e8f9269c2f5c10c3373de9
Eugene Butusov <EvgenyButusov@gmail.com>
2019-02-28 16:31:28 +0300

    Место проведения события #52
    добавлена загрузка скриптовой карты взамен статичной при загрузке API Яндекс Карт

--- components/EventLocation/EventLocation.tsx
+++ components/EventLocation/EventLocation.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
+import { useState } from 'react';
 import './EventLocation.css';
+import useScript from '../../lib/custom-hooks/useScript';
+
+declare global {
+    interface Window { ymaps: any; }
+}
+
+const API_KEY = '9e9809a5-fd4f-4f84-af9e-c17b2229db12';
+const MAP_ZOOM = 15;
 
 const getMapSrc = (lat: number, lng: number) =>
-    `https://static-maps.yandex.ru/1.x/?ll=${lat},${lng}&size=300,250&z=15&l=map&pt=${lat},${lng},org`;
+    `https://static-maps.yandex.ru/1.x/?ll=${lat},${lng}&size=300,250&z=${MAP_ZOOM}&l=map&pt=${lat},${lng},org`;
 
 export type EventLocationProps = {
     country: string;
@@ -15,17 +24,33 @@ export type EventLocationProps = {
 };
 
 export default function EventLocation(props: { location: EventLocationProps; }) {
-
     const { address, coordinates: { lat, lng } } = props.location;
+
+    const [ loaded ] = useScript(`https://api-maps.yandex.ru/2.1/?apikey=${API_KEY}&lang=ru_RU`);
+    const [ mapHandle, setMapHandle ] = useState(null);
+
+    if (loaded && !mapHandle) {
+        window.ymaps.ready(() => {
+            const map = new window.ymaps.Map('event-location-map', {
+                center: [ lng, lat ],
+                zoom: MAP_ZOOM
+            });
+            const myPlacemark = new window.ymaps.Placemark([ lng, lat ]);
+            map.geoObjects.add(myPlacemark);
+            setMapHandle(map);
+        });
+    }
+
     return (
         <div className="event-location">
             <small className="address-label text-muted font-italic d-block">{address}</small>
-
+            {!loaded &&
             <img
-                className="map-image"
+                className="event-location-map map-image"
                 alt={address}
                 src={getMapSrc(lat, lng)}
-            />
+            />}
+            {loaded && <div id="event-location-map" className="event-location-map" />}
         </div>
     );
 }
