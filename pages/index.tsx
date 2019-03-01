0fb8585e4621c4f7b0d32c8f6a4ef99971a75faa
neonick <n30n1ck@gmail.com>
2019-03-02 01:40:27 +0300

    Several fixes after code-review (in progress)

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const IndexPage: NextFunctionComponent = () => {
 
             <div className="container">
                 <div className="pb-2 mb-3 border-bottom">
-                    <h1 className="h2">Кто мы такие?</h1>
+                    <h1 className="h2">Krasnodar Dev Days</h1>
                 </div>
 
                 <div className="row">
@@ -27,7 +27,6 @@ const IndexPage: NextFunctionComponent = () => {
                             height="300px"
                             src="https://www.youtube.com/embed/IKiPtJEBWXE"
                             frameborder="0"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen
                         />
                     </div>
@@ -67,7 +66,7 @@ const IndexPage: NextFunctionComponent = () => {
                         />
                         <figcaption className="figure-caption">
                             <h4>
-                                <a href="https://vk.com/mmskvortsov">
+                                <a href="https://vk.com/mmskvortsov" target="_blank" rel="noopeneer">
                                     Михаил
                                     <br />
                                     Скворцов
@@ -85,7 +84,7 @@ const IndexPage: NextFunctionComponent = () => {
                         />
                         <figcaption className="figure-caption">
                             <h4>
-                                <a href="https://vk.com/n30n1ck">
+                                <a href="https://vk.com/n30n1ck" target="_blank" rel="noopeneer">
                                     Николай
                                     <br />
                                     Марченко
@@ -102,7 +101,7 @@ const IndexPage: NextFunctionComponent = () => {
                         />
                         <figcaption className="figure-caption">
                             <h4>
-                                <a href="https://vk.com/binakot">
+                                <a href="https://vk.com/binakot" target="_blank" rel="noopeneer">
                                     Иван
                                     <br />
                                     Муратов
@@ -119,7 +118,7 @@ const IndexPage: NextFunctionComponent = () => {
                         />
                         <figcaption className="figure-caption">
                             <h4>
-                                <a href="https://vk.com/tyvik">
+                                <a href="https://vk.com/tyvik" target="_blank" rel="noopeneer">
                                     Виктор
                                     <br />
                                     Тыщенко
@@ -136,7 +135,7 @@ const IndexPage: NextFunctionComponent = () => {
                         />
                         <figcaption className="figure-caption">
                             <h4>
-                                <a href="https://vk.com/marklangovoi">
+                                <a href="https://vk.com/marklangovoi" target="_blank" rel="noopeneer">
                                     Марк
                                     <br />
                                     Ланговой
@@ -152,38 +151,67 @@ const IndexPage: NextFunctionComponent = () => {
                 <div className="row">
                     <div className="col-xl-8">
                         <h2 className="h3">Где мы общаемся?</h2>
+                        <p>Вся коммуникация в сообществе происходит через Telegram.</p>
                         <p>
-                            Вся коммуникация в сообществе происходит через Telegram. Анонсы мероприятий мы делаем в
-                            соцсетях
+                            Анонсы мероприятий мы делаем в{' '}
+                            <a href="https://t.me/krddevdays" target="_blank" rel="noopeneer">
+                                основном канале
+                            </a>
+                            , в тематических чатах и в наших соцсетях.
                         </p>
                         <p>
-                            Если ты хотите задать вопрос организаторам, пообщаться с коллегами или поделиться опытом, у
-                            нас есть общий чат <a href="http://t.me/krddevdays_chat">@krddevdays_chat</a> и следующие
-                            чаты по интересам:
+                            Если вы хотите задать вопрос организаторам, пообщаться с коллегами или поделиться опытом, у
+                            нас есть общий чат{' '}
+                            <a href="http://t.me/krddevdays_chat" target="_blank" rel="noopeneer">
+                                @krddevdays_chat
+                            </a>{' '}
+                            и следующие чаты по интересам:
                         </p>
                         <ul>
                             <li>
-                                <a href="http://t.me/krdfrontend">@krdfrontend</a> для frontend-разработчиков;
+                                <a href="http://t.me/krdfrontend" target="_blank" rel="noopeneer">
+                                    @krdfrontend
+                                </a>{' '}
+                                для frontend-разработчиков;
                             </li>
                             <li>
-                                <a href="http://t.me/krdbackend">@krdbackend</a> для backend-разработчиков;
+                                <a href="http://t.me/krdbackend" target="_blank" rel="noopeneer">
+                                    @krdbackend
+                                </a>{' '}
+                                для backend-разработчиков;
                             </li>
                             <li>
-                                <a href="http://t.me/pythonkrd">@pythonkrd</a> для python-разработчиков;
+                                <a href="http://t.me/pythonkrd" target="_blank" rel="noopeneer">
+                                    @pythonkrd
+                                </a>{' '}
+                                для python-разработчиков;
                             </li>
                             <li>
-                                <a href="http://t.me/krdQA">@krdQA</a> для QA-инженеров;
+                                <a href="http://t.me/krdQA" target="_blank" rel="noopeneer">
+                                    @krdQA
+                                </a>{' '}
+                                для QA-инженеров;
                             </li>
                             <li>
-                                <a href="http://t.me/golangkrasnodar">@golangkrasnodar</a> для Go-разработчиков;
+                                <a href="http://t.me/golangkrasnodar" target="_blank" rel="noopeneer">
+                                    @golangkrasnodar
+                                </a>{' '}
+                                для Go-разработчиков;
                             </li>
                             <li>
-                                <a href="http://t.me/rubykrd">@rubykrd</a> для Ruby-разработчиков;
+                                <a href="http://t.me/rubykrd" target="_blank" rel="noopeneer">
+                                    @rubykrd
+                                </a>{' '}
+                                для Ruby-разработчиков;
                             </li>
                         </ul>
                         <p>
                             Основное правило – ни в одном из чатов выше нельзя публиковать вакансии и резюме, для этого
-                            есть <a href="http://t.me/krddevcareer">@krddevcareer</a>.
+                            есть{' '}
+                            <a href="http://t.me/krddevcareer" target="_blank" rel="noopeneer">
+                                @krddevcareer
+                            </a>
+                            .
                         </p>
                     </div>
 
@@ -191,18 +219,58 @@ const IndexPage: NextFunctionComponent = () => {
                         <h2 className="h3">Как с нами связаться?</h2>
                         <p>
                             По всем вопросам вы можете написать нам на почту{' '}
-                            <a href="mailto:help@krddevdays.ru">help@krddevdays.ru</a>
+                            <a href="mailto:help@krddevdays.ru">help@krddevdays.ru</a>.
+                        </p>
+                        <p>
+                            Подписывайтесь на нас в{' '}
+                            <a href="https://vk.com/krddevdays" target="_blank" rel="noopeneer">
+                                VK
+                            </a>
+                            ,{' '}
+                            <a href="https://www.facebook.com/krddevdays/" target="_blank" rel="noopeneer">
+                                Фейсбуке
+                            </a>
+                            ,{' '}
+                            <a href="https://twitter.com/krddevdays" target="_blank" rel="noopeneer">
+                                Твиттере
+                            </a>
+                            ,{' '}
+                            <a href="https://www.instagram.com/krddevdays/" target="_blank" rel="noopeneer">
+                                Инстаграме
+                            </a>{' '}
+                            и{' '}
+                            <a
+                                href="https://www.youtube.com/channel/UCBVZa_qgKhT8_MMqvR0bNQA"
+                                target="_blank"
+                                rel="noopeneer"
+                            >
+                                Ютубе
+                            </a>
+                            .
                         </p>
                         <p>
                             А если хотите у нас выступить, заполните{' '}
-                            <a href="https://connect.yandex.ru/forms/5adc61cf6162d77e2714831c/">короткую анкету</a>.
+                            <a
+                                href="https://connect.yandex.ru/forms/5adc61cf6162d77e2714831c/"
+                                target="_blank"
+                                rel="noopeneer"
+                            >
+                                короткую анкету
+                            </a>
+                            .
                         </p>
                     </div>
 
                     <div className="col">
                         <p>
-                            Подключайтесь, общайтесь, приходите на наши мероприятия, участвуйте в жизни сообщества,{' '}
-                            <a href="https://connect.yandex.ru/forms/5adc61cf6162d77e2714831c/">становитесь спикером</a>{' '}
+                            Подключайтесь, общайтесь, приходите на наши мероприятия, участвуйте в жизни сообщества,
+                            <a
+                                href="https://connect.yandex.ru/forms/5adc61cf6162d77e2714831c/"
+                                target="_blank"
+                                rel="noopeneer"
+                            >
+                                становитесь спикером
+                            </a>
                             и развивайтесь!
                         </p>
                     </div>
