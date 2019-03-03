9eec507623d36a5d6011923625ba0647193442a9
neonick <n30n1ck@gmail.com>
2019-03-03 23:28:29 +0300

    Fixes and codestyle

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,54 +3,38 @@ import { NextFunctionComponent } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 
-const organizators = [
+const orgs = [
     {
         name: 'Михаил Скворцов',
-        role: 'Сооснователь сообщества, главный помощник по всему.',
+        description: 'Сооснователь сообщества, главный помощник по всему.',
         link: 'https://vk.com/mmskvortsov',
-        img: 'https://via.placeholder.com/180x230'
+        img: 'https://via.placeholder.com/220x230'
     },
     {
         name: 'Николай Марченко',
-        role: 'Организатор митапов «Krasnodar Frontend».',
+        description: 'Организатор митапов «Krasnodar Frontend».',
         link: 'https://vk.com/n30n1ck',
-        img: 'https://via.placeholder.com/180x230'
+        img: 'https://via.placeholder.com/220x230'
     },
     {
         name: 'Иван Муратов',
-        role: 'Организатор митапов «Krasnodar Backend».',
+        description: 'Организатор митапов «Krasnodar Backend».',
         link: 'https://vk.com/binakot',
-        img: 'https://via.placeholder.com/180x230'
+        img: 'https://via.placeholder.com/220x230'
     },
     {
         name: 'Виктор Тыщенко',
-        role: 'Организатор митапов «Krasnodar Python.',
+        description: 'Организатор митапов «Krasnodar Python.',
         link: 'https://vk.com/tyvik',
-        img: 'https://via.placeholder.com/180x230'
+        img: 'https://via.placeholder.com/220x230'
     },
     {
         name: 'Марк Ланговой',
-        role: 'Основатель сообщества, организатор конференций «Krasnodar Dev Days»',
+        description: 'Основатель сообщества, организатор конференций «Krasnodar Dev Days»',
         link: 'https://vk.com/marklangovoi',
-        img: 'https://via.placeholder.com/180x230'
+        img: 'https://via.placeholder.com/220x230'
     }
 ];
-
-const getOrgsData = organizators.map(org => {
-    return (
-        <div className="card mb-3">
-            <img src={org.img} alt={org.name} />
-            <div className="card-body">
-                <h4 className="card-title">
-                    <a href={org.link} target="_blank" rel="noopeneer">
-                        {org.name}
-                    </a>
-                </h4>
-                <p className="card-text">{org.role}</p>
-            </div>
-        </div>
-    );
-});
 
 const IndexPage: NextFunctionComponent = () => {
     return (
@@ -71,14 +55,9 @@ const IndexPage: NextFunctionComponent = () => {
                             Создано, чтобы объединять, развивать и поддерживать всех, кто так или иначе причастен к
                             разработке.
                         </p>
-                        <iframe
-                            className="mb-3"
-                            width="100%"
-                            height="300px"
-                            src="https://www.youtube.com/embed/IKiPtJEBWXE"
-                            frameborder="0"
-                            allowfullscreen
-                        />
+                        <div className="embed-responsive embed-responsive-16by9 mb-3">
+                            <iframe src="https://www.youtube.com/embed/IKiPtJEBWXE" frameBorder="0" allowFullScreen />
+                        </div>
                     </div>
 
                     <div className="col-xl-4">
@@ -121,7 +100,23 @@ const IndexPage: NextFunctionComponent = () => {
             <div className="container mb-3">
                 <h2 className="h3 border-bottom pb-3 mb-4">Кто организаторы?</h2>
 
-                <div className="card-deck">{getOrgsData}</div>
+                <div className="d-flex flex-row flex-nowrap overflow-auto">
+                    {orgs.map((org, index) => {
+                        return (
+                            <div className="card mb-3 mr-3" key={index} style={{ minWidth: 210, maxWidth: 210 }}>
+                                <img src={org.img} alt={org.name} />
+                                <div className="card-body">
+                                    <h4 className="h5 card-title">
+                                        <a href={org.link} target="_blank" rel="noopeneer">
+                                            {org.name}
+                                        </a>
+                                    </h4>
+                                    <p className="card-text">{org.description}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
 
             <div className="container">
@@ -139,44 +134,44 @@ const IndexPage: NextFunctionComponent = () => {
                         <p>
                             Если вы хотите задать вопрос организаторам, пообщаться с коллегами или поделиться опытом, у
                             нас есть общий чат{' '}
-                            <a href="http://t.me/krddevdays_chat" target="_blank" rel="noopeneer">
+                            <a href="https://t.me/krddevdays_chat" target="_blank" rel="noopeneer">
                                 @krddevdays_chat
                             </a>{' '}
                             и следующие чаты по интересам:
                         </p>
                         <ul>
                             <li>
-                                <a href="http://t.me/krdfrontend" target="_blank" rel="noopeneer">
+                                <a href="https://t.me/krdfrontend" target="_blank" rel="noopeneer">
                                     @krdfrontend
                                 </a>{' '}
                                 для frontend-разработчиков;
                             </li>
                             <li>
-                                <a href="http://t.me/krdbackend" target="_blank" rel="noopeneer">
+                                <a href="https://t.me/krdbackend" target="_blank" rel="noopeneer">
                                     @krdbackend
                                 </a>{' '}
                                 для backend-разработчиков;
                             </li>
                             <li>
-                                <a href="http://t.me/pythonkrd" target="_blank" rel="noopeneer">
+                                <a href="https://t.me/pythonkrd" target="_blank" rel="noopeneer">
                                     @pythonkrd
                                 </a>{' '}
                                 для python-разработчиков;
                             </li>
                             <li>
-                                <a href="http://t.me/krdQA" target="_blank" rel="noopeneer">
+                                <a href="https://t.me/krdQA" target="_blank" rel="noopeneer">
                                     @krdQA
                                 </a>{' '}
                                 для QA-инженеров;
                             </li>
                             <li>
-                                <a href="http://t.me/golangkrasnodar" target="_blank" rel="noopeneer">
+                                <a href="https://t.me/golangkrasnodar" target="_blank" rel="noopeneer">
                                     @golangkrasnodar
                                 </a>{' '}
                                 для Go-разработчиков;
                             </li>
                             <li>
-                                <a href="http://t.me/rubykrd" target="_blank" rel="noopeneer">
+                                <a href="https://t.me/rubykrd" target="_blank" rel="noopeneer">
                                     @rubykrd
                                 </a>{' '}
                                 для Ruby-разработчиков.
@@ -185,7 +180,7 @@ const IndexPage: NextFunctionComponent = () => {
                         <p>
                             Основное правило – ни в одном из чатов выше нельзя публиковать вакансии и резюме, для этого
                             есть{' '}
-                            <a href="http://t.me/krddevcareer" target="_blank" rel="noopeneer">
+                            <a href="https://t.me/krddevcareer" target="_blank" rel="noopeneer">
                                 @krddevcareer
                             </a>
                             .
